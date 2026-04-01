2dfff86dc3de2bf305cfbd33e29d8c34e85d6c76
Patrick-Jambo <mateusz.p.wieckowski@gmail.com>
2026-04-01 22:44:48 +0200

    Add point inside obstacle movement logic

--- ATBPExtension/src/main/java/xyz/openatbp/extension/game/PathFinder.java
+++ ATBPExtension/src/main/java/xyz/openatbp/extension/game/PathFinder.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PathFinder {
+    public static float INTERSECTION_STOP_DISTANCE = 0.5f;
     private List<Obs> obstacleList;
     private final Path2D mapArea;
 
@@ -69,17 +70,18 @@ public class PathFinder {
         Line2D movementLine = new Line2D.Float(start, end);
 
         boolean intersection = false;
+        Point2D intersectionPoint = null;
         boolean isInside = false;
 
         for (Obs obs : obstacleList) {
             if (obs.getPath().contains(end)) {
                 isInside = true;
-                break;
             }
 
             for (Line2D edge : obs.getEdges()) {
                 if (edge.intersectsLine(movementLine)) {
                     intersection = true;
+                    intersectionPoint = getIntersectionPoint(movementLine, edge);
                     break;
                 }
             }
@@ -87,9 +89,60 @@ public class PathFinder {
         }
 
         List<Point2D> movePointsToDest = new ArrayList<>();
-        if (!intersection && !isInside) {
+        if (!intersection && !isInside && mapArea.contains(end)) {
             movePointsToDest.add(end);
         }
+
+        if (isInside
+                && intersection
+                && intersectionPoint != null
+                && mapArea.contains(intersectionPoint)) {
+            double distToIntersection = start.distance(intersectionPoint);
+
+            float lDist = (float) distToIntersection - INTERSECTION_STOP_DISTANCE;
+            Line2D line = Champion.getAbilityLine(start, intersectionPoint, lDist);
+            movePointsToDest.add(line.getP2());
+        }
+
         return movePointsToDest;
+    }
+
+    private double calculateDet(Point2D p1, Point2D p2) {
+        return (p1.getX() * p2.getY()) - (p2.getX() * p1.getY());
+    }
+
+    public Point2D getIntersectionPoint(Line2D line1, Line2D line2) {
+        // first point L1
+        double L1_x1 = line1.getX1();
+        double L1_y1 = line1.getY1();
+        Point2D p1 = new Point2D.Double(L1_x1, L1_y1);
+
+        // second point L1
+        double L1_x2 = line1.getX2();
+        double L1_y2 = line1.getY2();
+        Point2D p2 = new Point2D.Double(L1_x2, L1_y2);
+
+        // first point L2
+        double L2_x1 = line2.getX1();
+        double L2_y1 = line2.getY1();
+
+        // second point L2
+        double L2_x2 = line2.getX2();
+        double L2_y2 = line2.getY2();
+
+        Point2D x_Diff = new Point2D.Double(L1_x1 - L1_x2, L2_x1 - L2_x2);
+        Point2D y_Diff = new Point2D.Double(L1_y1 - L1_y2, L2_y1 - L2_y2);
+
+        double divisorDet = calculateDet(x_Diff, y_Diff);
+        if (divisorDet == 0) return null;
+
+        double L1_d = calculateDet(line1.getP1(), line1.getP2());
+        double L2_d = calculateDet(line2.getP1(), line2.getP2());
+
+        Point2D d = new Point2D.Double(L1_d, L2_d);
+
+        double x = calculateDet(d, x_Diff) / divisorDet;
+        double y = calculateDet(d, y_Diff) / divisorDet;
+        return new Point2D.Double(x, y);
     }
 }
